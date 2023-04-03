184c8e9f8c02cd10a6ecd2d266e357a831417bd0
Matheus Diniz Ferreira <67669247+mthsdiniz@users.noreply.github.com>
2023-04-03 13:10:21 -0300

    feat: new changes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from flask import Flask, render_template, jsonify, request
 import requests
-from flask import Flask, render_template, jsonify
+from urllib.parse import quote
+import re
 
 app = Flask(__name__)
 
@@ -7,32 +9,50 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/fetch-file')
+def fetch_file():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "URL not provided"}), 400
 
-@app.route('/geojson-files')
-def geojson_files():
-    owner = 'mthsdiniz'  # Replace with the owner of the GitHub repository
-    repo = 'geojsons'  # Replace with the name of the GitHub repository
-    # Use local
-    #github_api_url = f'https://api.github.com/repos/{owner}/{repo}/contents'
-    #response = requests.get(github_api_url)
-
-    # Use Enterprise
-    github_enterprise_base_url = 'https://your_github_enterprise_instance.com'  # Replace with your GitHub Enterprise base URL
-    github_api_url = f'{github_enterprise_base_url}/api/v3/repos/{owner}/{repo}/contents'
-
-    headers = {'Authorization': 'token your_access_token'}
-    response = requests.get(github_api_url, headers=headers)
-
+    response = requests.get(url)
     if response.status_code == 200:
-        files = response.json()
-        geojson_files = [
-            {'name': file['name'], 'url': file['download_url']}
-            for file in files
-            if file['name'].endswith('.geojson')
-        ]
-        return jsonify(geojson_files)
+        return response.content
     else:
-        return jsonify([])
+        return jsonify({"error": "Failed to fetch file"}), response.status_code
+
+@app.route('/get-files')
+def get_files():
+    # Replace with your own repository owner, repository name, and GitHub access token
+    owner = 'NewFrontiers'
+    repo = 'china_optical_pipelines'
+    access_token = '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'
+
+    # Set up headers with access token
+    headers = {'Authorization': f'token {access_token}'}
+
+    # Recursive call to get all files in repository
+    def get_files_recursive(path):
+        url = f'https://api.github.com/repos/{owner}/{repo}/contents/{quote(path)}'
+        response = requests.get(url, headers=headers)
+        print(response.status_code)
+        if response.status_code == 200:
+            files = []
+            for item in response.json():
+                if item['type'] == 'file':
+                    if re.match(r'.*\.geojson$|.*\.kml$', item['name']):
+                        files.append({
+                            'name': item['name'],
+                            'url': item['download_url']
+                        })
+                elif item['type'] == 'dir':
+                    files += get_files_recursive(item['path'])
+            return files
+        else:
+            return []
+
+    files = get_files_recursive('')
+    return jsonify(files)
 
 if __name__ == '__main__':
     app.run(debug=True)
